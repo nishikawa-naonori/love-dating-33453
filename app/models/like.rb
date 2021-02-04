f055e5886f960007f3f5c83b0d8f624ac669c0cb
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-02-04 16:00:50 +0900

    Revert "Update like.rb"
    This reverts commit dd8aa144ed5096dd503864afc0935690065746f5.

--- app/models/like.rb
+++ app/models/like.rb
@@ -8,10 +8,7 @@ class Like < ApplicationRecord
 
   # belongs_to :romance, counter_cache: :likes_count
 
-
-    # 一人のユーザーは一人の投稿にしか、いいねができない
-
   validates_uniqueness_of :romance_id, scope: :user_id
 
-  #sssssssssss
+
 end

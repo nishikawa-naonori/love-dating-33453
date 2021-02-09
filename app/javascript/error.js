1ad163893435e45aca5edb87f1290e8599ac8a6d
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-02-09 15:22:35 +0900

    2月9日 完成版

---
+++ app/javascript/error.js
@@ -0,0 +1,50 @@
+if (document.URL.match( /sign_up/ )) {
+  window.addEventListener('load',function(){
+    const nickName = document.getElementById("registration-nickname");
+    const eMail = document.getElementById("registration-email");
+    const passWord = document.getElementById("registration-password");
+    const passWordConfirmation = document.getElementById("registration-password-confirmation");
+
+    nickName.addEventListener("blur", e => { 
+      const nickNameError = document.getElementById("nickname-error")
+      if (nickName.value != ""){
+        nickNameError.setAttribute("hidden", true)
+      } else {
+        nickNameError.removeAttribute("hidden")
+      }
+    })
+
+    eMail.addEventListener("blur", e => { 
+      const eMailError = document.getElementById("email-error")
+      const eMailValidate = /^[a-z]+@[a-z]$/;
+
+      const eMailValidate = /^[a-z]+@[a-z]{1,}$/;
+
+      if (eMailValidate.test(eMail.value)){
+        eMailError.setAttribute("hidden", true)
+      } else {
+        eMailError.removeAttribute("hidden")
+      }
+    })
+
+    passWord.addEventListener("blur", e => { 
+      const passWordError = document.getElementById("password-error")
+      const passWordValidate = /^(?=.*[a-z])(?=.*[0-9])[0-9a-z]{6,}$/;
+      if (passWordValidate.test(passWord.value)){
+        passWordError.setAttribute("hidden", true)
+      } else {
+        passWordError.removeAttribute("hidden")
+      }
+    })
+
+    passWordConfirmation.addEventListener("blur", e => { 
+      const passWordConfirmationError = document.getElementById("password-confirmation-error")
+      if (passWord.value == passWordConfirmation.value){
+        passWordConfirmationError.setAttribute("hidden", true)
+      } else {
+        passWordConfirmationError.removeAttribute("hidden")
+      }
+    })
+
+  });
+}

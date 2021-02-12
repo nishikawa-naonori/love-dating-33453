5388390e3e4ce6cb57c2a7ed0b8b0f334c6bf86c
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-02-12 11:09:16 +0900

    2月12日 1回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,30 +2,30 @@ class User < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :validatable
+
     belongs_to :seibetu
     belongs_to :work
 
     has_many :comments, dependent: :destroy
   
-    validates :nickname, presence: true
-  
-   # has_many :likes, dependent: :destroy
     has_many :likes, dependent: :destroy
     
     has_many :romances, dependent: :destroy
 
-    # has_many :liked_romances, through: :likes, source: :romance
-  
-   # def already_liked?(romance)
-    # self.likes.exists?(romance_id: romance.id) 
-  #  end
+#バリデーション----------------------------------------------
+    # ニックネーム
+    validates :nickname, presence: true
+
+  # パスワードを半角英数字にバリデーション
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英数字の両方を含めて設定してください'
+
+
 
     def liked_by?(romance_id)
       likes.where(romance_id: romance_id).exists?
     end
 
-  #  def liked_by?(post_id)
-   #   likes.where(post_id: post_id).exists?
-  #  end
+
   end
   

2a47ef50342508a6c516aa39a88a0261454af8ee
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-02-12 11:18:23 +0900

    2月11日 2回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,15 @@ class User < ApplicationRecord
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英数字の両方を含めて設定してください'
 
+      # ジャンルの選択が「--」の時は保存できないようにする
+  with_options numericality: { other_than: 1 , message: 'を選んでください'} do
+    validates :seibetu_id
+  end
+
+  with_options numericality: { other_than: 1 , message: 'を選んでください'} do
+    validates :work_id
+  end
+
 
 
     def liked_by?(romance_id)
